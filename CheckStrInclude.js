dcb77f737d6c6aa75d484c750c4684838ab4997b
Avneesh Singh Yadav <yadav.asy88@gmail.com>
2025-08-03 21:23:31 +0530

    include fn related

--- CheckStrInclude.js
+++ CheckStrInclude.js
@@ -1,9 +1,19 @@
 
 
-let str="I am Avneeesh sing yadav";
-const CheckStrInclude=(str,subStr)=>{
-    return str.toLowerCase().includes(subStr.toLowerCase())?true : false
-}
+// let str="I am Avneeesh sing yadav";
+// const CheckStrInclude=(str,subStr)=>{
+//     return str.toLowerCase().includes(subStr.toLowerCase())?true : false
+// }
 
 
-console.log("Is the string include avneesh: "+CheckStrInclude("I am Avneesh sing yadav","avneesh"))
+// console.log("Is the string include avneesh: "+CheckStrInclude("I am Avneesh sing yadav","avneesh"))
+
+let arr=["avneesh","eaersh","avn"]
+let first =arr[0]
+let second =arr[2]
+
+  if(first.includes(second)){
+    console.log("The first string includes the second string")
+  }else{
+    console.log("Not includes")
+  }
