1419ca236cae2e0fa679c2ee04fe01ee0a00baf9
Avneesh Singh Yadav <yadav.asy88@gmail.com>
2025-09-17 21:28:56 +0530

    array

--- Array/ReverseArray.js
+++ Array/ReverseArray.js
@@ -1,9 +1,27 @@
 
 
- const arr= [1,2,3,4,5]
+ const arr= [1,2,9,3,4,5]
 
-    let newArr=[]
-    for(var i=arr.length-1; i>=0 ;i--){
-        newArr.push(arr[i])
+    // let newArr=[]
+    // for(var i=arr.length-1; i>=0 ;i--){
+    //     newArr.push(arr[i])
+    // }
+    // console.log("The reversed array is :"+ newArr) 
+
+
+
+
+    //  Arrange in ascending order
+    for(let i=0 ;i<arr.length;i++){
+        for(let j=i+1;j<arr.length;j++){
+
+            if(arr[i]>arr[j]){
+                let temp=arr[i]
+                arr[i]=arr[j]
+                arr[j]=temp
+
+            }
+        }
+    
     }
-    console.log("The reversed array is :"+ newArr) 
+     console.log(arr)
